356f64f3721183bc4e5f20fa656d04f88898ca29
watcharapol-numpaya <59914403+watcharapol-numpaya@users.noreply.github.com>
2021-04-29 01:48:24 +0700

    Update v1_script_create_tableDB.sql

--- v1_script_create_tableDB.sql
+++ v1_script_create_tableDB.sql
@@ -5,22 +5,22 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema SmartStock
+-- Schema SmartWebStore
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `SmartStock` ;
+DROP SCHEMA IF EXISTS `SmartWebStore` ;
 
 -- -----------------------------------------------------
--- Schema SmartStock
+-- Schema SmartWebStore
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `SmartStock` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
-USE `SmartStock` ;
+CREATE SCHEMA IF NOT EXISTS `SmartWebStore` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE `SmartWebStore` ;
 
 -- -----------------------------------------------------
--- Table `SmartStock`.`brands`
+-- Table `SmartWebStore`.`brands`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `SmartStock`.`brands` ;
+DROP TABLE IF EXISTS `SmartWebStore`.`brands` ;
 
-CREATE TABLE IF NOT EXISTS `SmartStock`.`brands` (
+CREATE TABLE IF NOT EXISTS `SmartWebStore`.`brands` (
   `brandCode` INT NOT NULL AUTO_INCREMENT,
   `brandName` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`brandCode`))
@@ -28,11 +28,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `SmartStock`.`products`
+-- Table `SmartWebStore`.`products`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `SmartStock`.`products` ;
+DROP TABLE IF EXISTS `SmartWebStore`.`products` ;
 
-CREATE TABLE IF NOT EXISTS `SmartStock`.`products` (
+CREATE TABLE IF NOT EXISTS `SmartWebStore`.`products` (
   `prodCode` INT NOT NULL AUTO_INCREMENT,
   `prodName` VARCHAR(50) NOT NULL,
   `prodPrice` DECIMAL(8,2) NOT NULL,
@@ -45,18 +45,18 @@ CREATE TABLE IF NOT EXISTS `SmartStock`.`products` (
   INDEX `fk_BrandCode_idx` (`fkBrandCode` ASC) VISIBLE,
   CONSTRAINT `fkBrandCode`
     FOREIGN KEY (`fkBrandCode`)
-    REFERENCES `SmartStock`.`brands` (`brandCode`)
+    REFERENCES `SmartWebStore`.`brands` (`brandCode`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `SmartStock`.`colors`
+-- Table `SmartWebStore`.`colors`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `SmartStock`.`colors` ;
+DROP TABLE IF EXISTS `SmartWebStore`.`colors` ;
 
-CREATE TABLE IF NOT EXISTS `SmartStock`.`colors` (
+CREATE TABLE IF NOT EXISTS `SmartWebStore`.`colors` (
   `colCode` INT NOT NULL AUTO_INCREMENT,
   `colName` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`colCode`))
@@ -64,11 +64,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `SmartStock`.`producthascolors`
+-- Table `SmartWebStore`.`producthascolors`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `SmartStock`.`producthascolors` ;
+DROP TABLE IF EXISTS `SmartWebStore`.`producthascolors` ;
 
-CREATE TABLE IF NOT EXISTS `SmartStock`.`producthascolors` (
+CREATE TABLE IF NOT EXISTS `SmartWebStore`.`producthascolors` (
   `prodHasColorCode` INT NOT NULL AUTO_INCREMENT,
   `prodCode` INT NOT NULL,
   `colCode` INT NOT NULL,
@@ -77,12 +77,12 @@ CREATE TABLE IF NOT EXISTS `SmartStock`.`producthascolors` (
   PRIMARY KEY (`prodHasColorCode`),
   CONSTRAINT `fk_PRODUCTS_has_COLOR_PRODUCTS1`
     FOREIGN KEY (`prodCode`)
-    REFERENCES `SmartStock`.`products` (`prodCode`)
+    REFERENCES `SmartWebStore`.`products` (`prodCode`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_PRODUCTS_has_COLOR_COLOR1`
     FOREIGN KEY (`colCode`)
-    REFERENCES `SmartStock`.`colors` (`colCode`)
+    REFERENCES `SmartWebStore`.`colors` (`colCode`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
