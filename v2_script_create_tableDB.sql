640743dd7dd837aa817a24e0f5dbc23b5a6d3ffe
watcharapol-numpaya <59914403+watcharapol-numpaya@users.noreply.github.com>
2021-04-28 15:57:08 +0700

    Add files via upload
    script_create_table

---
+++ v2_script_create_tableDB.sql
@@ -0,0 +1,111 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema SmartStock
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `SmartStock` ;
+
+-- -----------------------------------------------------
+-- Schema SmartStock
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `SmartStock` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE `SmartStock` ;
+
+-- -----------------------------------------------------
+-- Table `SmartStock`.`brands`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SmartStock`.`brands` ;
+
+CREATE TABLE IF NOT EXISTS `SmartStock`.`brands` (
+  `brandCode` INT NOT NULL AUTO_INCREMENT,
+  `brandName` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`brandCode`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SmartStock`.`products`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SmartStock`.`products` ;
+
+CREATE TABLE IF NOT EXISTS `SmartStock`.`products` (
+  `prodCode` INT NOT NULL AUTO_INCREMENT,
+  `prodName` VARCHAR(50) NOT NULL,
+  `prodPrice` DECIMAL(8,2) NOT NULL,
+  `prodDescription` VARCHAR(300) NOT NULL,
+  `prodDateManufacturer` DATE NOT NULL,
+  `prodWaranty` INT NOT NULL,
+  `fkBrandCode` INT NOT NULL,
+  PRIMARY KEY (`prodCode`, `fkBrandCode`),
+  INDEX `fk_PRODUCTS_Brand1_idx` (`fkBrandCode` ASC) VISIBLE,
+  CONSTRAINT `fk_PRODUCTS_Brand1`
+    FOREIGN KEY (`fkBrandCode`)
+    REFERENCES `SmartStock`.`brands` (`brandCode`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SmartStock`.`colors`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SmartStock`.`colors` ;
+
+CREATE TABLE IF NOT EXISTS `SmartStock`.`colors` (
+  `colCode` INT NOT NULL AUTO_INCREMENT,
+  `colName` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`colCode`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SmartStock`.`producthascolors`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SmartStock`.`producthascolors` ;
+
+CREATE TABLE IF NOT EXISTS `SmartStock`.`producthascolors` (
+  `prodHasColorCode` INT NOT NULL AUTO_INCREMENT,
+  `prodCode` INT NOT NULL,
+  `colCode` INT NOT NULL,
+  INDEX `fk_PRODUCTS_has_COLOR_COLOR1_idx` (`colCode` ASC) VISIBLE,
+  INDEX `fk_PRODUCTS_has_COLOR_PRODUCTS1_idx` (`prodCode` ASC) VISIBLE,
+  PRIMARY KEY (`prodHasColorCode`),
+  CONSTRAINT `fk_PRODUCTS_has_COLOR_PRODUCTS1`
+    FOREIGN KEY (`prodCode`)
+    REFERENCES `SmartStock`.`products` (`prodCode`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PRODUCTS_has_COLOR_COLOR1`
+    FOREIGN KEY (`colCode`)
+    REFERENCES `SmartStock`.`colors` (`colCode`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `SmartStock`.`images`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `SmartStock`.`images` ;
+
+CREATE TABLE IF NOT EXISTS `SmartStock`.`images` (
+  `imageCode` INT NOT NULL AUTO_INCREMENT,
+  `image` VARCHAR(100) NOT NULL,
+  `fkProdCode` INT NOT NULL,
+  PRIMARY KEY (`imageCode`),
+  INDEX `fk_images_products1_idx` (`fkProdCode` ASC) VISIBLE,
+  CONSTRAINT `fk_images_products1`
+    FOREIGN KEY (`fkProdCode`)
+    REFERENCES `SmartStock`.`products` (`prodCode`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
